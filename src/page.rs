754425c2abc73f38a514210470c06f46632d9cf7
Lekha Sathvik Devabathini <dlsathvik04@gmail.com>
2026-07-07 18:39:13 +0530

    implement the decompression algorithm and complete initial implementation of the cups raster format parsing

--- src/page.rs
+++ src/page.rs
@@ -1,6 +1,6 @@
 use std::io::{Read, Result};
 
-use crate::{cups_reader::CUPSReader, page, page_header::PageHeader};
+use crate::{cups_reader::CUPSReader, page_header::PageHeader};
 
 /// represents a single page in the whole data.
 /// contains the raser format verson, a header and
@@ -31,21 +31,37 @@ impl DecompressedPage {
         };
 
         let pixel_values: Vec<u8> = match &header {
-            PageHeader::V1(_) | PageHeader::V3(_) => {
-                reader.read_n_bytes(page_body_size as usize)?
-            }
+            PageHeader::V1(_) | PageHeader::V3(_) => reader.read_n_bytes(page_body_size)?,
             PageHeader::V2(_) => {
-                let mut pixel_values: Vec<u8> = Vec::with_capacity(page_body_size as usize);
+                let mut pixel_values: Vec<u8> = Vec::with_capacity(page_body_size);
 
-                while pixel_values.len() < pixel_values.capacity() {
-                    let repeat_byte = reader.read_byte()?;
-                    if repeat_byte < 128 {
-                    } else {
+                let unit_bytes = match cups_color_order {
+                    0 => (base.cups_bits_per_pixel + 7).div_ceil(8),
+                    _ => (base.cups_bits_per_color + 7).div_ceil(8),
+                };
+
+                while page_body_size > pixel_values.len() {
+                    let line_rep_byte = reader.read_byte()? as u16 + 1;
+                    let mut current_line: Vec<u8> = Vec::with_capacity(bytes_per_line);
+                    while current_line.len() < bytes_per_line {
+                        let control_byte = reader.read_byte()?;
+                        if control_byte < 128 {
+                            let color_repeat_count = control_byte + 1;
+                            let color_bytes = reader.read_n_bytes(unit_bytes as usize)?;
+                            for _ in 0..color_repeat_count {
+                                current_line.extend_from_slice(&color_bytes);
+                            }
+                        } else {
+                            let literal_unit_count = 257 - control_byte as u16;
+                            let literal_bytes = reader
+                                .read_n_bytes(literal_unit_count as usize * unit_bytes as usize)?;
+                            current_line.extend_from_slice(&literal_bytes);
+                        }
+                    }
+                    for _ in 0..line_rep_byte {
+                        pixel_values.extend_from_slice(&current_line);
                     }
                 }
-
-                // TODO : Implement the decompression algorithm
-
                 pixel_values
             }
         };
